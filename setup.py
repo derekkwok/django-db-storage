e1c5c183e44cbdcd0d8d554ddbe99b44481a8b17
derekkwok <derek.kai.chun.kwok@gmail.com>
2016-02-28 13:50:53 -0500

    Release 1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-db-storage',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/derekkwok/django-db-storage',
     license='BSD',
     author='Derek Kwok',
